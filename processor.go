7a4f518f148a7cfc7e23e1ec2b25dbf3598bb66d
Leo <lpguercio@gmail.com>
2020-09-11 00:56:38 +0200

    Adding Statistics struct and basic associated methods

--- processor.go
+++ processor.go
@@ -4,16 +4,51 @@ import (
 	"context"
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 const LOG_FILE = "./numbers.log"
+
+var terminate = fmt.Sprintf("%s%s", "terminate", LINE_BREAK)
+var currOs = runtime.GOOS
 
 type Appender interface {
 	AddToLog(ctx context.Context, path, input string) error
 }
 
-var terminate = fmt.Sprintf("%s%s", "terminate", LINE_BREAK)
-var currOs = runtime.GOOS
+type Statistics struct {
+	sync.Mutex
+	Received   int
+	Duplicates int
+	Total      int
+}
+
+// Prints to STDOUT the current statistics of the server,
+// regarding received numbers, number of duplicates and
+// total number of unique numbers received by the server
+func (s *Statistics) PrintCurrent() {
+	s.Lock()
+	fmt.Printf("Received: %d unique numbers, %d duplicates, "+
+		"Unique totals: %d \n", s.Received, s.Duplicates, s.Total)
+	s.Unlock()
+}
+
+// Updates Total statistics based on the values recv and dups,
+// Received and Duplicates. If one of them is negative, it
+// silently exits
+func (s *Statistics) Update(recv, dups int) {
+	if recv < 0 || dups < 0 {
+		return
+	}
+	if dups > recv {
+		return
+	}
+	s.Lock()
+	defer s.Unlock()
+	s.Received = recv
+	s.Duplicates = dups
+	s.Total = s.Total + (recv - dups)
+}
 
 // Basic Validation functions
 
--- processor_test.go
+++ processor_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,8 +15,81 @@ type validateInputTestCase struct {
 	ErrorMsg string
 }
 
+type statisticsTestCase struct {
+	Name       string
+	Received   int
+	Duplicates int
+	Expected   int
+}
+
+func TestStatistics(t *testing.T) {
+	t.Run("Update Basic Correctness", func(t *testing.T) {
+		errMsg := "Got %d, Expected: %d"
+		testCases := []statisticsTestCase{
+			{
+				Name:       "No change",
+				Received:   1,
+				Duplicates: 1,
+				Expected:   100,
+			},
+			{
+				Name:       "Negative Received",
+				Received:   -1,
+				Duplicates: 30,
+				Expected:   100,
+			},
+			{
+				Name:       "Negative Duplicates",
+				Received:   20,
+				Duplicates: -2,
+				Expected:   100,
+			},
+			{
+				Name:       "Changed 1",
+				Received:   33,
+				Duplicates: 13,
+				Expected:   120,
+			},
+			{
+				Name:       "Changed 2",
+				Received:   456,
+				Duplicates: 56,
+				Expected:   500,
+			},
+		}
+		for _, tc := range testCases {
+			t.Run(tc.Name, func(t *testing.T) {
+				s := &Statistics{Total: 100}
+				s.Update(tc.Received, tc.Duplicates)
+				assert.True(t, tc.Expected == s.Total, errMsg, s.Total, tc.Expected)
+			})
+		}
+	})
+
+	t.Run("Update Many", func(t *testing.T) {
+		s := &Statistics{Total: 100}
+		asserter := func(recv, dups int) bool {
+			curr := s.Total
+			if recv < 0 || dups < 0 {
+				return curr == s.Total
+			}
+			if dups > recv {
+				return curr == s.Total
+			}
+			if s.Total > 999999999 {
+				return true
+			}
+			s.Update(recv, dups)
+			return s.Total > 0
+		}
+		if err := quick.Check(asserter, &quick.Config{MaxCount: 100}); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
 func TestValidateInputNum(t *testing.T) {
-	t.Run("Validate Input Num", func(t *testing.T) {
+	t.Run("Validate Input", func(t *testing.T) {
 		testCases := []validateInputTestCase{
 			{
 				Name:     "Valid Input Num 1",
