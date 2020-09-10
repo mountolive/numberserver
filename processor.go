f7a51b4296ce1bab3731bd0efcacb2ac9ee8399b
Leo <lpguercio@gmail.com>
2020-09-10 20:40:47 +0200

    Adding carriage config. Adding test cases for CheckTermination

--- processor.go
+++ processor.go
@@ -1,5 +1,10 @@
 package main
 
+func CheckTermination(input string) bool {
+	// TODO Implement
+	return false
+}
+
 func ValidateInputNum(input string) bool {
 	// TODO Implement
 	return false
--- processor_test.go
+++ processor_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type validateInputNumCase struct {
+type inputTestCase struct {
 	Name     string
 	Input    string
 	Expected bool
@@ -14,8 +15,8 @@ type validateInputNumCase struct {
 }
 
 func TestProcessor(t *testing.T) {
-	t.Run("Validate Input", func(t *testing.T) {
-		testCases := []validateInputNumCase{
+	t.Run("Validate Input Num", func(t *testing.T) {
+		testCases := []inputTestCase{
 			{
 				Name:     "Valid Input Num 1",
 				Input:    "314159265",
@@ -49,8 +50,42 @@ func TestProcessor(t *testing.T) {
 		}
 		for _, tc := range testCases {
 			t.Run(tc.Name, func(t *testing.T) {
-				result := ValidateInputNum(tc.Input)
+				input := fmt.Sprintf("%s%s", tc.Input, LINE_BREAK)
+				result := ValidateInputNum(input)
 				assert.True(t, tc.Expected == result, tc.ErrorMsg, tc.Input, tc.Expected)
+			})
+		}
+	})
+
+	t.Run("Check Termination", func(t *testing.T) {
+		generalErrorMsg := "The passed word %v should have prompted %v"
+		testCases := []inputTestCase{
+			{
+				Name:     "Correct termination",
+				Input:    "terminate",
+				Expected: true,
+			},
+			{
+				Name:     "Not termination",
+				Input:    "anotherword",
+				Expected: false,
+			},
+			{
+				Name:     "Composed, bad 1",
+				Input:    "terminate hello",
+				Expected: false,
+			},
+			{
+				Name:     "Composed, bad 2",
+				Input:    "hello terminate",
+				Expected: false,
+			},
+		}
+		for _, tc := range testCases {
+			t.Run(tc.Name, func(t *testing.T) {
+				input := fmt.Sprintf("%s%s", tc.Input, LINE_BREAK)
+				result := CheckTermination(input)
+				assert.True(t, tc.Expected == result, generalErrorMsg, tc.Input, tc.Expected)
 			})
 		}
 	})
